50173249d2f4cd0b10dfef02370a49cc018dfe20
Beau <bmb0121@auburn.edu>
2025-04-05 20:14:36 -0500

    Test commit for Git Hook

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-print('Hello, world!')
-exit (1)
+# test.py
+import pickle
+import os
+
+def insecure_function():
+    # Insecure use of pickle
+    data = pickle.loads("malicious_data")
+    return data
+
+def run_command():
+    # Insecure use of os.system
+    os.system("rm -rf /")
